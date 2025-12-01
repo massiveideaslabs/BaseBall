7f1e2024123744ab5c627d33c3330e52ce15e13b
Derzathon <andersen.scherberger@gmail.com>
2025-12-02 00:08:59 +0800

    Exclude scripts and contracts from Next.js build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: false,
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
@@ -10,6 +17,11 @@ const nextConfig = {
         tls: false,
       };
     }
+    // Exclude scripts and contracts from webpack
+    config.module.rules.push({
+      test: /\.ts$/,
+      exclude: /(scripts|contracts|hardhat\.config\.ts)/,
+    });
     return config;
   },
 };
